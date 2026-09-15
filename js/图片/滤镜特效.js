e1cfb65d5f19773a891be5d680562d2b4980d76c
bxhrdhr <166931481+bxhrdhr@users.noreply.github.com>
2026-09-15 12:16:27 +0800

    Add js/图片/滤镜特效.js

---
+++ js/图片/滤镜特效.js
@@ -0,0 +1,5 @@
+(function() {
+    var s = document.createElement('style');
+    s.textContent = ".card img, .lightbox img { filter: contrast(1.2) saturate(1.1); transition: filter 0.5s ease; }";
+    document.head.appendChild(s);
+})();
