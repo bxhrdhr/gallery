6641df48d811c5e40177c103e22b765fc6009a42
bxhrdhr <166931481+bxhrdhr@users.noreply.github.com>
2026-09-16 11:38:53 +0800

    特效层级统一降到查看器之下，放大图片不被遮挡

--- js/全屏/樱花特效.js
+++ js/全屏/樱花特效.js
@@ -122,7 +122,7 @@ case 'fnr':
             cxt;
         canvas.height = window.innerHeight;
         canvas.width = window.innerWidth;
-        canvas.setAttribute('style', 'position: fixed;left: 0;top: 0;pointer-events: none;');
+        canvas.setAttribute('style', 'position: fixed;left: 0;top: 0;pointer-events: none; z-index: 5;');
         canvas.setAttribute('id', 'canvas_sakura');
         document.getElementsByTagName('body')[0].appendChild(canvas);
         cxt = canvas.getContext('2d');
